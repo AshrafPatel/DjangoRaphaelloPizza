a258ac709c6a4148185d630886da67b7067f5e00
AshrafPatel <4shr4f@live.co.uk>
2025-12-23 14:17:26 +0000

    React project has removed and cleaned up some desirable for deployment

--- raphaellopizzas/settings.py
+++ raphaellopizzas/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
-SECRET_KEY = "yellow"
+SECRET_KEY = [os.environ["DJANGO_SECRET_KEY"] / "yellow"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -148,5 +148,3 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "myorders/static"),
     os.path.join(BASE_DIR, "users/static")
 )
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
